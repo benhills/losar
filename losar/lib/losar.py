23cfbac476c5a63fc03b82eb21ffc333f2021bec
Benjamin Hills <benjaminhhills@gmail.com>
2024-01-15 23:25:03 -0800

    Add full comments on pythong scripts and jupyter notebook

--- losar/lib/losar.py
+++ losar/lib/losar.py
@@ -10,8 +10,8 @@ import numpy as np
 
 from scipy.ndimage import gaussian_filter as gf
 
-from losar.lib.sar_functions import (doppler_centroid, layer_optimization,
-                                     get_optimal_frequencies)
+from losar.lib.sar_functions import (losar_doppler, losar_stack,
+                                     get_optimal_wavenumbers)
 
 
 def losar(image, N,
@@ -22,30 +22,55 @@ def losar(image, N,
           gf_window=[5, 5],
           verbose=False):
     """
+    The full LoSAR algorithm for an ice-penetrating radar profile.
+    Based on Castelletti et al. (2019)
+    The core of the processing functions are in a separate script.
 
+    Parameters
+    -----------
+    image           2d array; input image
+    N               int; SAR aperture (number of traces)
+    nus             1d array; wavenumbers
+    dx              float; trace separation (distance)
+    layer_finding   string; choice of slope finding algorithm
+    gaussian_filter bool; decide to smooth the stacked power/wavenumber image
+    gf_window       list; smoothing window
+    verbose         bool; print output
+
+    Output
+    -----------
+    losar_image     3d array; 2 images the size of the input image
+                                first is the stacked power
+                                second is the extracted slopes
     """
 
+    # shape of input image, num_samples, num_traces
     snum, tnum = np.shape(image)
 
+    # pre-filled output array
     losar_image = np.empty((2, snum, tnum))
 
+    # Loop through all traces
     for tidx in np.arange(tnum):
-
-        if verbose:
+        if verbose:  # print trace number; show the loop is going
             print(tidx, end=' ')
+        # subset of the image with the aperture length around the given trace
+        image_sub = image[:, max(0, tidx-N//2): min(tnum, tidx+N//2)]
 
-        image_sub = image[:, max(0,tidx-N//2): min(tnum, tidx+N//2)]
-
+        # Get stacked power using one of the imported losar functions
         if layer_finding == 'doppler':
-            P, nus = doppler_centroid(image_sub, dx=dx)
+            P, nus = losar_doppler(image_sub, dx=dx)
         elif layer_finding == 'stack':
-            P = layer_optimization(image_sub, nus)
+            P = losar_stack(image_sub, nus)
 
+        # Smooth the stacked power image
         if gaussian_filter:
             P = gf(P, gf_window)
 
-        p_best, f_best = get_optimal_frequencies(P, nus)
+        # Get the 'best' layer dip and power stacked along that dip
+        p_best, f_best = get_optimal_wavenumbers(P, nus)
 
+        # Save the stacked power and layer dip to output array
         losar_image[0, :, tidx] = p_best
         losar_image[1, :, tidx] = f_best
 
--- losar/lib/sar_functions.py
+++ losar/lib/sar_functions.py
@@ -15,83 +15,119 @@ from .supplemental import dB
 
 def incoherent_average(image):
     """
+    Incoherent stack (amplitude only, no phase tracking)
 
+    Parameters
+    -----------
+    image           2d array (snum,tnum); data image to stack
+
+    Output
+    -----------
+    stacked_image   2d array (snum,tnum)
     """
-    return np.sum(abs(image)**2.,axis=1) / np.shape(image)[1]
+
+    return np.sum(abs(image)**2., axis=1) / np.shape(image)[1]
 
 
 def coherent_summation(image, phi=0.):
     """
     Sum the complex image across traces, coherently (account for phase)
 
+    Parameters
+    -----------
+    image           2d array (snum,tnum); data image to stack
+    phi             float; phase shift between traces
 
+    Output
+    -----------
+    stacked_image   2d array (snum,tnum)
     """
-    snum,tnum = np.shape(image)
 
-    if phi != 0.: # If phi is 0 there is no rotation so skip
+    snum, tnum = np.shape(image)
+
+    if phi != 0.:  # If phi is 0 there is no rotation so skip
         # phase offset for each trace
-        phi_traces = np.linspace(tnum/2.*phi,-tnum/2.*phi,tnum)
+        phi_traces = np.linspace(tnum/2.*phi, -tnum/2.*phi, tnum)
         # get the amplitude and phase of the image
         R = abs(image)
         theta = np.angle(image)
         # add a phase offset based on trace distance from center of aperture
-        theta += np.tile(phi_traces,(snum,1))
+        theta += np.tile(phi_traces, (snum, 1))
         # recalculate the complex image from amplitude and phase
         image = R*np.exp(1j*theta)
 
     return np.sum(image, axis=1)
 
 
-def layer_optimization(image,phis):
+def losar_stack(image, phis):
     """
-
-    """
-    snum,tnum = np.shape(image)
-    p_stack = np.empty((0,snum))
-    for phi in phis:
-        p_single = coherent_summation(image,phi=phi)
-        p_stack = np.append(p_stack,[p_single],axis=0)
-    return np.transpose(p_stack)
-
-
-def doppler_centroid(image,dx):
-    """
-    Get the doppler centroid for an image (typically for one synthetic aperture, not the entire radar profile)
+    Option 1 of 2 for the core of the LoSAR algorithm
+    Coherent sum, stacking across a range of viable dips
+    Castelletti et al. (2019)
 
     Parameters
     -----------
-    image   data image within a single synthetic aperture
-    dx      spatial step between traces
+    image       2d array (snum,tnum); data image to stack
+    phis        1d array; phase offsets
 
     Output
     -----------
-    p_dop   power
-    freq    frequencies
+    p_stack     2d array; stacked power for all depths and at all phis
     """
-    snum,tnum = np.shape(image)
-    p_dop = np.empty((0,tnum-1))
+
+    snum, tnum = np.shape(image)
+    P_stack = np.empty((0, snum))
+    for phi in phis:  # TODO: convert to wavenumbers
+        P_single = coherent_summation(image, phi=phi)
+        P_stack = np.append(P_stack, [P_single], axis=0)
+    return np.transpose(P_stack)
+
+
+def losar_doppler(image, dx):
+    """
+    Option 2 of 2 for the core of the LoSAR algorithm
+    Get the periodograms relative to doppler centroid for an image
+    MacGregor et al. (2015) (Radiostratigraphy of Greenland)
+
+    Parameters
+    -----------
+    image       2d array (snum,tnum); data image to stack
+    dx          spatial step between traces
+
+    Output
+    -----------
+    P_dop       2d array; power from periodogram
+    nus         1d array; sorted wavenumbers
+    """
+    snum, tnum = np.shape(image)
+    P_dop = np.empty((0, tnum-1))
     for i in range(snum):
-        freq, power = periodogram(image[i],fs=1./dx)
-        idx = np.argsort(freq[1:])
-        p_dop = np.append(p_dop,[power[1:][idx]],axis=0)
-    return p_dop,freq[idx]
+        nus, power = periodogram(image[i], fs=1./dx)
+        idx = np.argsort(nus[1:])
+        P_dop = np.append(P_dop, [power[1:][idx]], axis=0)
+    return P_dop, nus[idx]
 
 
-def get_optimal_frequencies(image,f_dop,threshold=-190.):
+def get_optimal_wavenumbers(P, nus_full, threshold=-190.):
     """
+    Get the 'best' wavenumbers based on the most stacked power
+    from one of the functions above.
 
     Parameters
     -----------
-    image   data image within a single synthetic aperture
+    P           2d array; Stacked power (from one of the losar functions above)
+    nus_full    1d array; wavenumbers
+    threshold   float; cutoff value for stacked power
+                        (low power assign slope as nan)
 
     Output
     -----------
-    f   frequencies
-    p   power
+    p_best      power
+    nu_best   frequencies
     """
-    snum,tnum = np.shape(image)
-    f = f_dop[np.argmax(image,axis=1)]
-    p = np.array([np.max(image[i]) for i in range(snum)])
-    f[dB(p)<threshold] = np.nan
+    snum, N = np.shape(P)
+    nu_best = nus_full[np.argmax(P, axis=1)]
+    p_best = np.array([np.max(P[i]) for i in range(snum)])
+    nu_best[dB(p_best) < threshold] = np.nan
 
-    return p,f
+    return p_best, nu_best
